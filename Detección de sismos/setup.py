cb2f4442a729fa3dda98a26484324fe2b2301c3a
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2025-05-29 02:42:57 -0300

    armado cookiecuter

---
+++ Detección de sismos/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='se realizara un proyecto orientado a la detección de sismos con datos de la base astronómica',
+    author='Zalazar Martin, Quinteros Nicolas, Sanabria Luciano y Mansilla gustavo',
+    license='MIT',
+)
